ea0608127b47257dbf34ecd06dc60dbfbf723c86
SumeetThombre <Sumeet.Thombre@student.lut.fi>
2015-09-19 18:02:59 +0200

    Version 3 : with server side command execution

--- src/SocketClientHandler.java
+++ src/SocketClientHandler.java
@@ -10,11 +10,12 @@
  */
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,7 +23,7 @@ public class SocketClientHandler implements Runnable {
 
 	private Socket client;
         SocketClientHandler g;
-        StringBuffer output = null;
+        String output = null;
         
 	public SocketClientHandler(Socket client) {
 		this.client = client;
@@ -35,10 +36,24 @@ public class SocketClientHandler implements Runnable {
 			System.out.println("Thread started with name:"+Thread.currentThread().getName());
                     try {
                         readResponse();
-                        sendOutput(output);
+                        
                     } catch (IOException | InterruptedException ex) {
                         Logger.getLogger(SocketClientHandler.class.getName()).log(Level.SEVERE, null, ex);
                     }
+                    //try {
+                        
+                        //client.close();
+                        //try {
+                        //System.out.println(output);
+                        //sendOutput(output);
+                        
+                        //} catch (IOException | InterruptedException ex) {
+                        //  Logger.getLogger(SocketClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                        //}
+                    //} catch (IOException ex) {
+                      //  Logger.getLogger(SocketClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                    //}
+                    
 		} /*catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 		}*/
@@ -46,27 +61,32 @@ public class SocketClientHandler implements Runnable {
         
         private void readResponse() throws IOException, InterruptedException {
 		String userInput;
-		BufferedReader stdIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                System.out.println("hej");
+		DataInputStream stdIn = new DataInputStream(new DataInputStream(client.getInputStream()));
+                //System.out.println(stdIn.toString());
                 //System.out.println(stdIn.readLine());
-		while ((userInput = stdIn.readLine()) != null) {
+		userInput = stdIn.readUTF();
 			//if(userInput.equals("TIME?")){
 				System.out.println("REQUEST TO EXECUTE SYSTEM COMMAND");
-                                userInput = stdIn.readLine();
-                                System.out.println(userInput);
-                                ExecuteCommand execute = new ExecuteCommand(g,userInput); 
+                                //userInput = stdIn.readLine();
+                                //System.out.println(userInput);
+                                //g = new SocketClientHandler(client);
+                                ExecuteCommand execute = new ExecuteCommand(this,userInput); 
                                 execute.start();
 				//sendOutput();
-				break;
-			}
-			
+				//break;
 		//}
-                System.out.println("hej");
+                //System.out.println("hej");
 	}
                 
-        void sendOutput(StringBuffer output) throws IOException, InterruptedException {
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-                writer.write(output.toString());
+        void sendOutput(String output) throws IOException, InterruptedException {
+                DataOutputStream out2Client = new DataOutputStream(new DataOutputStream(client.getOutputStream()));
+                //System.out.println("SCH :" + output);
+                //String o = output.toString();
+                out2Client.writeUTF(output);
+                //writer.newLine();
+                //writer.flush();
+                //writer.close();
+                
         }
 }
         
